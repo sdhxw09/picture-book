ab98333a7710e66550ad63fa3679455ee92e3b49
U-RAILINC\sdhxw09 <sdhxw09@RAIL0131.railinc.com>
2012-02-03 19:30:06 -0500

    completed content script

--- content.js
+++ content.js
@@ -1,6 +1,9 @@
 //find all images larger than 500 x 384
-$('img').each(function () {
-    if (this.width > 500 && this.height > 384) {
-        Console.log(this.src);
-    }
+chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+    $('img').each(function () {
+        if (this.width > 500 && this.height > 384) {
+            chrome.extension.sendRequest({imageUrl: this.src});
+        }
+    });
 });
+
